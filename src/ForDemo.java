e09f62b76de7808dd8e44fdb661996cc58daca6d
webdevbrent <webdeveloperbrent@gmail.com>
2018-09-18 09:58:55 -0400

    First commit on ForDemo

---
+++ src/ForDemo.java
@@ -0,0 +1,9 @@
+/* Demonstrate the for loop */
+public class ForDemo {
+    public static void main(String args[]){
+        int count;
+        for(count = 0; count < 5; count++)
+            System.out.println("This is a count: " + count);
+        System.out.println("Done!");
+    }
+}
